cd6e8885205b317a23e90062169bd3552856847b
Pratik <pratiksangani.siliconithub@gmail.com>
2023-02-22 14:15:08 +0530

    goal controller and end route setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
